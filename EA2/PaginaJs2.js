ef009e5772d20b921d8f48d8417c3e978cebacf1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-20 11:55:24 -0400

    UPDATE

--- EA2/PaginaJs2.js
+++ EA2/PaginaJs2.js
@@ -3,8 +3,14 @@ function sumar()
     // value obtiene el valor actual del input
     let n1 = document.getElementById("numero1").value;
     let n2 = document.getElementById("numero2").value;
+    if(n1 == "")
+    {
+        alert("Valor no especificado");
+        return;
+    }
+    
     // parseInt => convierte un string en int
-    let total = parseInt(n1) + parseInt(n2);
+    let total = parseInt("0" + n1) + parseInt("0" + n2);
     alert("El resultado es: " + total);
 }
 
@@ -12,3 +18,38 @@ function sumar()
 // Ejercicios: Agregar botones y funciones para realizar:
 // resta, multiplicación y división (evitar dividir por cero).
 // y que no esten vacios
+
+function restar()
+{
+    // value obtiene el valor actual del input
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    // parseInt => convierte un string en int
+    let total = parseInt(n1) - parseInt(n2);
+    alert("El resultado es: " + total);
+}
+function multiplicar()
+{
+    // value obtiene el valor actual del input
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    // parseInt => convierte un string en int
+    let total = parseInt(n1) * parseInt(n2);
+    alert("El resultado es: " + total);
+}
+function dividir()
+{
+    // value obtiene el valor actual del input
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    // parseInt => convierte un string en int
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+    if(n2 == 0)
+    {
+        alert("No se puede dividir por cero.");
+        return;
+    }
+    alert("El resultado es: " + n1/n2);
+}
