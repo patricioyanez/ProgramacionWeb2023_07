ae18606242adc448b9bcdde8ac32cf7c19fe3033
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-01 09:23:55 -0400

    update

--- EA3/instituto/crud/admin.py
+++ EA3/instituto/crud/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Marca ## importar los modelos a usar
+from .models import Marca, Categoria, Genero, Cliente ## importar los modelos a usar
 # Register your models here.
 
 admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Genero)
+admin.site.register(Cliente)
--- EA3/instituto/crud/models.py
+++ EA3/instituto/crud/models.py
@@ -9,6 +9,38 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Categoria(models.Model):
+    idCategoria= models.AutoField(db_column='idCategoria', primary_key=True)
+    nombre = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Genero(models.Model):
+    idGenero= models.AutoField(db_column='idGenero', primary_key=True)
+    nombre = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):    
+    rut = models.IntegerField(primary_key=True, unique=True, null=False, blank=False)
+    dv = models.CharField(max_length=1, null=False, blank=False, default="")
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    direccion = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    telefono = models.CharField(max_length=20, null=False, blank=False)
+    genero = models.ForeignKey(Genero, db_column='idGenero', 
+                                on_delete=models.CASCADE, null=False, blank=False)
+    fechaNacimiento = models.DateField(null=False, blank=False)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
 
 #Pasos para migrar modelos personalizados:
 #1.- Preparar migración
