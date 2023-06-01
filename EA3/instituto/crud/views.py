af7ce792d303b461ca6f18b8646f5ef49754da5d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-01 11:18:52 -0400

    update

--- EA3/instituto/crud/views.py
+++ EA3/instituto/crud/views.py
@@ -4,4 +4,28 @@ from .models import Marca
 # Create your views here.
 
 def marca(request):
-    return render(request, 'marca.html')
+    context = {}
+    if request.method == 'POST': # si hay solicitud POST
+        id = int("0" + request.POST['id']) # convierte en numero
+        nombre = request.POST['nombre']
+        activo = 'activo' in request.POST # checkeado es True, False si no
+
+        if 'Grabar' in request.POST:
+            if id == 0: #insert
+                Marca.objects.create(nombre=nombre, activo=activo)
+                context = {'exito': 'Los datos fueron guardados'}
+            else:
+                try:
+                    item = Marca.objects.get(pk = id)
+                    item.nombre = nombre
+                    item.activo = activo
+                    item.save()
+                    context = {'exito': 'Los datos fueron modificados'}
+                except:
+                    context = {'error': 'No se pudo encontrar el registro'}
+
+        elif 'Listar' in request.POST:
+            listado = Marca.objects.all()
+            context = {'listado': listado}
+
+    return render(request, 'marca.html', context)
