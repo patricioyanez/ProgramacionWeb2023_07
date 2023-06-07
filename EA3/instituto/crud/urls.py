d95456d02682f66846283fac45adaf6ab824531e
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-07 11:08:16 -0400

    update

--- EA3/instituto/crud/urls.py
+++ EA3/instituto/crud/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('marca', views.marca, name='marca'),
+    path('categoria', views.categoria, name='categoria'),
 ]
 # Ejercicios: Realizar el crud para categoria y genero
+# Ejercicios: Crear Modelo de producto y migrar
 # 127.0.0.1:8000/crud/marca
--- EA3/instituto/crud/views.py
+++ EA3/instituto/crud/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
 # importar modelos
-from .models import Marca
+from .models import Marca, Categoria
 # Create your views here.
+
+def categoria(request):
+    context = {}
+    if request.method == 'POST': # si hay solicitud POST
+        id = int("0" + request.POST['id']) # convierte en numero
+        nombre = request.POST['nombre']
+        activo = 'activo' in request.POST # checkeado es True, False si no
+
+        if 'Grabar' in request.POST:
+            if id == 0: #insert
+                Categoria.objects.create(nombre=nombre, activo=activo)
+                context = {'exito': 'Los datos fueron guardados'}
+            else:
+                try:
+                    item = Categoria.objects.get(pk = id)
+                    item.nombre = nombre
+                    item.activo = activo
+                    item.save()
+                    context = {'exito': 'Los datos fueron modificados'}
+                except:
+                    context = {'error': 'No se pudo encontrar el registro'}
+
+        elif 'Listar' in request.POST:
+            listado = Categoria.objects.all() # select * from categoria
+            context = {'listado': listado}
+
+        elif 'Buscar' in request.POST:
+            try: # select * from categoria where idcategoria = X
+                item = Categoria.objects.get(pk = id) # 1 fila de la tabla 
+                context = {'item': item }
+            except:
+                context = {'error': 'No se pudo encontrar el registro'}
+        elif 'Eliminar' in request.POST:
+            try:
+                item = Categoria.objects.get(pk = id) # 1 fila de la tabla 
+                item.delete() # delete
+                context = {'exito': 'El id fue eliminado'}
+            except:
+                context = {'error': 'No se pudo encontrar el registro'}
+
+
+    return render(request, 'categoria.html', context)
 
 def marca(request):
     context = {}
